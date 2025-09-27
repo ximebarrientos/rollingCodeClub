9298228838e27057cfd5a1b91b4c228e2dd1c5b0
Claudia Ximena Barrientos <cxbarrientos1@gmail.com>
2025-09-27 18:07:25 -0300

    agregue input de nombre, precio, descripcion y una categoria al formulario producto

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import FormularioProducto from "./components/pages/producto/FormularioProducto"
 
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <h1>Rolling Code Club</h1>
+      <FormularioProducto></FormularioProducto>
     </>
   )
 }
--- src/components/pages/producto/FormularioProducto.jsx
+++ src/components/pages/producto/FormularioProducto.jsx
@@ -1,11 +1,45 @@
-
+import { Container, Form, Row } from "react-bootstrap";
 
 const FormularioProducto = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <Container>
+      <Row className="justify-content-center">
+        <Form>
+          <h2>Cargar producto</h2>
+          <Form.Group className="mb-3" controlId="nombreProducto">
+            <Form.Label>Nombre del producto</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Ingrese el nombre del producto"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="precioProducto">
+            <Form.Label>Precio</Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="Ingrese el precio del producto"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="descripcionProducto">
+            <Form.Label>Descripción</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={3}
+              placeholder="Ingrese una descripción del producto"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="categoriaProducto">
+            <Form.Label>Categoría</Form.Label>
+            <Form.Select>
+              <option value="">Seleccionar</option>
+              <option value="indumentaria">Indumentaria</option>
+              <option value="articulos">Artículos para entrenar</option>
+            </Form.Select>
+          </Form.Group>
+        </Form>
+      </Row>
+    </Container>
+  );
 };
 
 export default FormularioProducto;
