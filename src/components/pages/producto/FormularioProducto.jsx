9d66e1b7380fb9aba94041fd29d7067ef8c04309
Claudia Ximena Barrientos <cxbarrientos1@gmail.com>
2025-09-27 18:38:57 -0300

    mejoré el formulario de producto añadiendo estilos y un botón de carga

--- src/components/pages/producto/FormularioProducto.jsx
+++ src/components/pages/producto/FormularioProducto.jsx
@@ -1,16 +1,17 @@
-import { Container, Form, Row } from "react-bootstrap";
+import { Container, Form, Row, Button } from "react-bootstrap";
 
 const FormularioProducto = () => {
   return (
     <Container>
-      <Row className="justify-content-center">
+      <Row className="justify-content-center mb-5">
         <Form>
-          <h2>Cargar producto</h2>
+          <h2 className="text-success text-center display-5">Cargar producto</h2>
           <Form.Group className="mb-3" controlId="nombreProducto">
             <Form.Label>Nombre del producto</Form.Label>
             <Form.Control
               type="text"
               placeholder="Ingrese el nombre del producto"
+              className="bg-primary text-light"
             />
           </Form.Group>
           <Form.Group className="mb-3" controlId="precioProducto">
@@ -18,11 +19,12 @@ const FormularioProducto = () => {
             <Form.Control
               type="number"
               placeholder="Ingrese el precio del producto"
+              className="bg-primary text-light"
             />
           </Form.Group>
           <Form.Group className="mb-3" controlId="categoriaProducto">
             <Form.Label>Categoría</Form.Label>
-            <Form.Select>
+            <Form.Select className="bg-primary text-light">
               <option value="">Seleccionar</option>
               <option value="Indumentaria">Indumentaria</option>
               <option value="Articulos">Artículos para entrenar</option>
@@ -30,7 +32,7 @@ const FormularioProducto = () => {
           </Form.Group>
           <Form.Group className="mb-3" controlId="subcategoriaProducto">
             <Form.Label>Tipo de Indumentaria</Form.Label>
-            <Form.Select >
+            <Form.Select className="bg-primary text-light">
               <option value="">Seleccionar</option>
               <option value="Remera">Remera</option>
               <option value="Short">Short</option>
@@ -39,7 +41,7 @@ const FormularioProducto = () => {
           </Form.Group>
           <Form.Group className="mb-3" controlId="imagenProducto">
             <Form.Label>Imagen del producto</Form.Label>
-            <Form.Control type="file" />
+            <Form.Control type="file" className="bg-primary text-light" />
           </Form.Group>
           <Form.Group className="mb-3" controlId="descripcionProducto">
             <Form.Label>Descripción</Form.Label>
@@ -47,8 +49,14 @@ const FormularioProducto = () => {
               as="textarea"
               rows={3}
               placeholder="Ingrese una descripción del producto"
+              className="bg-primary text-light"
             />
           </Form.Group>
+          <div className="d-grid">
+            <Button variant="success" type="submit">
+              Cargar producto
+            </Button>
+          </div>
         </Form>
       </Row>
     </Container>
