158fa21c31a635b7ca41a39e65649a9c925f42ca
Alessandra Borges <aleborges110900@gmail.com>
2025-09-27 01:53:12 -0300

    Agregue la imagen del logo al navbar y le agregue algunas clases a los botones

--- src/components/shared/Menu.jsx
+++ src/components/shared/Menu.jsx
@@ -6,8 +6,8 @@ export default function Menu() {
       <div className="container">
        
         <NavLink to="/" className="navbar-brand d-flex align-items-center gap-2">
-          {/* imagen aqui*/}
-          <span className="fw-bold">RCC</span>
+          <img src="/logopngblanco.png" alt="Logo Rolling Code Club" height="100" /> 
+          <span className="fw-bold"></span>
         </NavLink>
 
         <button
@@ -24,7 +24,7 @@ export default function Menu() {
 
         <div className="collapse navbar-collapse" id="mainNavbar">
 
-          <ul className="navbar-nav mx-lg-auto mb-2 mb-lg-0">
+          <ul className="navbar-nav mx-lg-auto mb-2 mb-lg-0 gap-2">
             <li className="nav-item">
               <NavLink to="/" end className={({isActive}) => `nav-link${isActive ? ' active' : ''}`}>
                 Inicio
@@ -76,8 +76,8 @@ export default function Menu() {
           </ul>
 
           <div className="d-flex align-items-center gap-2">
-            <NavLink to="/login" className="btn btn-outline-light btn-sm">Iniciar sesión</NavLink>
-            <NavLink to="/crear-cuenta" className="btn btn-primary btn-sm">Crear cuenta</NavLink>
+            <NavLink to="/login" className="btn btn-outline-light btn-sm py-3">Iniciar sesión</NavLink>
+            <NavLink to="/crear-cuenta" className="btn btn-primary btn-sm py-2">Crear cuenta</NavLink>
           </div>
         </div>
       </div>
