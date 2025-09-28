612988ecb02fafb1c0273c8502b5eb62ba6dc8ab
Alessandra Borges <aleborges110900@gmail.com>
2025-09-28 03:50:39 -0300

    He creado una carpeta de css para error404. Le di estilos a la imagen y lo finalicé.

--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -3,18 +3,20 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import Image from "react-bootstrap/Image";
+import "./error404.css"; 
 
 export default function Error404() {
   return (
     <Container className="text-center py-5">
       <Row className="justify-content-center">
         <Col md={8}>
-
-          <Image
-            src="/imgerror404.png"
-            alt="Error 404"
-            className="mb-4" height={700}
-          />
+          <div className="mt-2 mb-2">
+            <Image
+              src="/imgerror404.png"
+              alt="Error 404"
+              className="error-image"
+            />
+          </div>
 
           <h2 className="mb-3">Página no encontrada</h2>
           <p className="text-secondary mb-4">
