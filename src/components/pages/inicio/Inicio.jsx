76824047dcdeecba52f10a8ff00f5cb7e15ba48f
Alessandra Borges <aleborges110900@gmail.com>
2025-09-30 04:03:01 -0300

    Finalicé el maquetado de inicio

--- src/components/pages/inicio/Inicio.jsx
+++ src/components/pages/inicio/Inicio.jsx
@@ -3,12 +3,14 @@ import CarrouselInicio from "./CarrouselInicio";
 import SacarTurnoInicio from "./SacarTurnoInicio";
 import SponsorsInicio from "./SponsorsInicio";
 import ProductosInicio from "./ProductosInicio";
+import BienvenidaInicio from "./BienvenidaInicio";
 
 export default function Inicio() {
   return (
     <div className="bg-dark text-light py-5">
       <Container>
         <CarrouselInicio />
+        <BienvenidaInicio/>
         <SacarTurnoInicio />
         <ProductosInicio />
        <SponsorsInicio /> 
