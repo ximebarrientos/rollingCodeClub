483495383416038c3ea906734e8ff79e1126ce5f
Claudia Ximena Barrientos <cxbarrientos1@gmail.com>
2025-10-20 00:51:35 -0300

    cambie la ruta del boton de sacar turno dle inicio

--- src/components/pages/inicio/SacarTurnoInicio.jsx
+++ src/components/pages/inicio/SacarTurnoInicio.jsx
@@ -1,12 +1,13 @@
 import { Row, Col, Button } from "react-bootstrap";
 import "./inicio.css";
+import { Link } from "react-router";
 
 export default function SacarTurnoInicio() {
   return (
     <Row className="mt-5 mb-5">
       <Col className="bg-success p-4 rounded text-center sombra-verde">
         <h2 className="fw-bold">Saca tu turno aquí</h2>
-        <Button variant="dark" size="lg" className="mt-3" href="/reserva">
+        <Button as={Link} to={"/reserva"} variant="dark" size="lg" className="mt-3">
           Reservar turno
         </Button>
       </Col>
